029c499a3d720e92ccdf3f274b6773c4ef2af393
Kazuroni <sayajinbr30@gmail.com>
2021-11-01 17:24:47 -0300

    paulada

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Maior2Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1;
+            int numero2;
+
+            Console.Write("A soma do numero: ");
+            numero1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Mais..: ");
+            numero2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine($"é igual a: {numero1 + numero2}");
+            Console.WriteLine();
+            
+            Console.WriteLine("Digite qualquer tecla para finalizar...");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
